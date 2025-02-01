fe01bd0ae96f07c55c3431710344ce511c366822
Ronak99 <punase.ronak99@gmail.com>
2025-02-01 20:41:28 +0530

    Commented in background beams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const words = ["reliable", "beautiful", "functional", "tested"];
   return (
     <>
-      {/* <BackgroundBeams /> */}
+      <BackgroundBeams />
 
       <Header showSideBorder={false} />
 
